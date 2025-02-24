5e501d7340beacebaa0f177e131d41ea8716d7c8
admodev <admodevcodes@outlook.com>
2025-02-23 22:26:35 -0300

    feat: :sparkles: create login page3

---
+++ src/app/login/page.tsx
@@ -0,0 +1,16 @@
+/**
+ *
+ * @returns
+ */
+const LoginPage = () => {
+  return (
+    <div className="w-full h-screen">
+      <div className="container mx-auto w-1/3 h-1/3 flex flex-col justify-center items-center">
+        <h1 className="text-xl">FortexCRM</h1>
+        <h2 className="mt-5">Welcome back!</h2>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
